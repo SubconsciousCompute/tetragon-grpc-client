ac54427bd1afa103e8e3583dd618e2820c5afaff
Dilawar Singh <dilawar@subcom.tech>
2024-03-14 13:49:11 +0530

    chore: Adds proto file to source

--- tetragon-grpc/build.rs
+++ tetragon-grpc/build.rs
@@ -1,11 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     match tonic_build::configure().build_server(false).compile(
         &[
-            "../proto_files/capabilities.proto",
-            "../proto_files/tetragon.proto",
-            "../proto_files/stack.proto",
-            "../proto_files/events.proto",
-            "../proto_files/sensors.proto",
+            "../proto_files/tetragon/capabilities.proto",
+            "../proto_files/tetragon/tetragon.proto",
+            "../proto_files/tetragon/stack.proto",
+            "../proto_files/tetragon/events.proto",
+            "../proto_files/tetragon/sensors.proto",
         ],
         &["../proto_files"],
     ) {
