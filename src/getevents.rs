c0e14d077273251f74b25f9c63ecf11909a23ff3
Dilawar Singh <dilawar@subcom.tech>
2024-03-14 15:44:09 +0530

    chore: Now i need a tokio runtime

--- src/getevents.rs
+++ src/getevents.rs
@@ -1,20 +1,19 @@
 //! getevents.
 
-use futures::executor::block_on;
-use tokio::net::UnixStream;
-use crossbeam_channel::Sender;
-use std::path::{PathBuf};
-use tonic::transport::{Channel, Endpoint, Uri};
-use tower::service_fn;
 use crate::fine_guidance_sensors_client::FineGuidanceSensorsClient;
 use crate::{Filter, GetEventsRequest, GetEventsResponse};
+use crossbeam_channel::Sender;
+use futures::executor::block_on;
+use std::path::PathBuf;
+use tokio::net::UnixStream;
+use tonic::transport::{Channel, Endpoint, Uri};
+use tower::service_fn;
 
 /// Subscribe to tetragon events.
 pub struct EventsSubscriber {
     socket_path: PathBuf,
     tx: Sender<GetEventsResponse>,
 }
-
 
 impl EventsSubscriber {
     /// Create a new EventsSubscriber  that writes events to tx.
@@ -25,7 +24,7 @@ impl EventsSubscriber {
 
     /// Run the subscriber.
     pub fn run(&mut self) -> anyhow::Result<()> {
-        let mut client = block_on(Self::socket_client(self.socket_path.clone()))?;
+        let mut client = Self::socket_client(self.socket_path.clone())?;
         // NOTE: creating a request for getevents
         let request = tonic::Request::new(GetEventsRequest {
             allow_list: vec![Filter::default()],
@@ -45,23 +44,23 @@ impl EventsSubscriber {
     }
 
     /// Unix domain socket client
-    pub async fn socket_client(path: PathBuf) -> anyhow::Result<FineGuidanceSensorsClient<Channel>> {
+    fn socket_client(path: PathBuf) -> anyhow::Result<FineGuidanceSensorsClient<Channel>> {
         tracing::info!("Creating socket channel");
 
         // NOTE: We will ignore this uri because uds do not use it
         // if your connector does use the uri it will be provided
         // as the request to the `MakeConnection`.
-        let channel = Endpoint::try_from("http://0.0.0.0:9999")?
-            .connect_with_connector(service_fn(move |_: Uri| {
-                // Connect to a Uds socket
-                UnixStream::connect(path.clone())
-            }))
-        .await?;
+        let channel = block_on(
+            Endpoint::try_from("http://0.0.0.0:9999")?.connect_with_connector(service_fn(
+                move |_: Uri| {
+                    // Connect to a Uds socket
+                    UnixStream::connect(path.clone())
+                },
+            )),
+        )?;
 
         tracing::info!("Creating socket client");
         let client = FineGuidanceSensorsClient::new(channel);
         Ok(client)
     }
-
-
 }
